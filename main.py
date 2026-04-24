e353a73b46020bc8a9e6c6b823012933384647b2
Yu Jin <qldls0307@naver.com>
2026-04-24 17:20:21 +0900

    feat: list로 코드 개선

--- main.py
+++ main.py
@@ -6,31 +6,29 @@ def solution(n):
         before = result[0]
         cnt = 0
 
-        arr = "" #이번에 새로 만들
-        for idx,s in enumerate(result):
+        arr = [] #이번에 새로 만들
+        for s in result:
             #s의 개수 + s
-
 
             if s == before: #이전과 같으면 cnt +=1
                 # print("같",s)
                 cnt += 1
             else:
                 # print("달",s)
-                arr += str(cnt) + (str(before)) #이전거 붙여야한다
+                arr.append(str(cnt))
+                arr.append(before)#이전거 붙여야한다
 
                 cnt =1 #리셋
                 before = s
 
-        arr += str(cnt) + (str(s)) #마지막 거는 안들어가서 따로 붙여줌
-        result = arr
+        arr.append(str(cnt))
+        arr.append(s)
 
-        # print(i, result)
+        result = "".join(arr)
+        # print(result)
 
-    # 2. 가운데 두 자리수만 리턴한다
-    n = len(result)
-    idx = n//2
-    result = list(result)
-    return int("".join(result[idx-1:idx+1]))
+    mid = len(result) // 2
+    return int(result[mid - 1:mid + 1])
 
 if __name__ == '__main__':
     print(solution(5))
